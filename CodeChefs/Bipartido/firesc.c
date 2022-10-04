1b14ed2c47fc55cd23af4de5f4b084c799bdc8db
Diego-Notebook <2001.vieira.diego@gmail.com>
2022-10-04 13:05:54 -0300

    adicionado exercicio bipartido codechefs(incompleto)

---
+++ CodeChefs/Bipartido/firesc.c
@@ -0,0 +1,96 @@
+/*
+https://www.codechef.com/problems/FIRESC?tab=statement
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Vertice{
+    int visitado;
+    int tamanho_lista;
+    int lista_adj[1000];
+}Vertice;
+
+Vertice *aloca_vertice(int qtd_vertices){
+    Vertice *vertice;
+    vertice = (Vertice *)calloc(qtd_vertices+1,sizeof(Vertice));
+    vertice->visitado = 0;
+    return vertice;
+}
+
+void insere_aresta(Vertice *vertice, int origem, int destino){
+    vertice[origem].lista_adj[vertice[origem].tamanho_lista] = destino;
+    vertice[origem].tamanho_lista++;
+    vertice[destino].lista_adj[vertice[destino].tamanho_lista] = origem;
+    vertice[destino].tamanho_lista++;
+}
+void dfs(Vertice *vertice, int raiz){
+    if(vertice[raiz].visitado != 0){
+        return;
+    }
+    vertice[raiz].visitado = 1;
+    for(int i = 0; i < vertice[raiz].tamanho_lista; i++){
+        if(vertice[vertice[raiz].lista_adj[i]].visitado == 0){
+            dfs(vertice,vertice[raiz].lista_adj[i]);
+        }
+    }
+}
+int main(int argc, char const *argv[]){
+    Vertice *vertice;
+    int qtd_testes;
+    int qtd_vertices, qtd_arestas;
+    int origem, destino;
+    int max_rotas,max_caps;
+    int *cont_caps;
+    int cont_visitados;
+    scanf("%d %d", &qtd_vertices, &qtd_arestas);
+    vertice = aloca_vertice(qtd_vertices);
+    cont_caps = (int*)calloc(qtd_vertices, sizeof(int));
+    for(int i = 0 ; i < qtd_arestas ; i++){
+        scanf("%d %d", &origem,&destino);
+        insere_aresta(vertice,origem,destino);
+    }
+    max_rotas = 0;
+    cont_visitados = 0;
+    for(int i = 1; i <= qtd_vertices; i++){
+        if(vertice[i].visitado == 0){
+            dfs(vertice,i);
+            max_rotas++;
+            printf("\n");
+            for(int j = 1; j <= qtd_vertices;j++){
+                printf("%d ",vertice[j].visitado);
+            }
+            for(int j = 1; j <= qtd_vertices;j++){
+                if(vertice[j].visitado == 1){
+                    cont_caps[i-1]++;
+                }
+            }
+            if (i > 1){
+                cont_caps[i] = cont_caps[i] - cont_caps[i-1];
+            }
+            printf("\n");
+
+        }
+    }
+
+    printf("\nFINAL\n");
+    for(int i = 1; i <= qtd_vertices;i++){
+        printf("%d ",vertice[i].visitado);
+    }
+    printf("\n");
+
+    max_caps = 1;
+    
+    for(int i = 0; i < qtd_vertices ; i++){
+        printf("%d ",cont_caps[i]);
+    }
+    for(int i = 0 ; i < qtd_vertices;i++){
+        if(cont_caps[i] != 0)
+            max_caps = max_caps*cont_caps[i];
+    }
+
+
+    printf("\n%d %d\n", max_rotas, max_caps);
+    printf("\n");
+    
+    return 0;
+}
